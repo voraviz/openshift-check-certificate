393f22c3f08a846a47c90f4c3831131c06b9134b
voravitl <voravitl@gmail.com>
2020-11-12 15:00:53 +0700

    Remove kubelet-client that is older than 30 days

--- check-all-certificates.sh
+++ check-all-certificates.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 #oc get secret --all-namespaces|grep -i "kubernetes.io/tls"|awk '{print "echo \"\n========== certificate "$2" in "$1" namespace ==========\n \";""oc get secret "$2" -n "$1" -o json| jq -r .data[\\\"tls.crt\\\"]| base64 -d | openssl x509 -in /dev/stdin -text -noout|egrep -i \"Subject:|Issuer:|Validity|Not\""}' > check_certificate.sh
 OS=$(uname)
+output(){
+    if [ $# -gt 0 ];
+        then
+            if [ $1 == "csv" ];
+            then
+                printf "%s\n" "$PROJECT,$SECRET,$($DATE -d @$START_EPOCH),$($DATE -d @$END_EPOCH),$DAY_REMAIN"
+            fi
+        else
+            printf "%s\n" "==============================================="
+            printf "Project: %s\n" $PROJECT
+            printf "Secret: %s\n" $SECRET
+            printf "Created at: %s\n" "$($DATE -d @$START_EPOCH)"
+            printf "Expired after: %s\n" "$($DATE -d @$END_EPOCH)"
+            printf "Day remaining %s \n" $DAY_REMAIN
+        fi
+}
 if [ $OS == "Darwin" ];
 then
     which gdate 1>/dev/null 2>&1
@@ -15,7 +31,6 @@ else
     DATE=date
 fi
 NOW_EPOCH=$($DATE +"%s")
-echo $NOW
 for PROJECT in $(oc get projects --no-headers|grep 'openshift-'|awk '{print $1}')
 do
     for SECRET in $(oc get secret -n $PROJECT|grep -i 'kubernetes.io/tls'|awk '{print $1}'|sort -r)
@@ -32,19 +47,19 @@ do
         START_EPOCH=$($DATE --date="${START_DATE}" +"%s")
         DIFF=$(expr $END_EPOCH - $NOW_EPOCH)
         DAY_REMAIN=$(expr $DIFF / 86400)
-        if [ $# -gt 0 ];
+        CREATE_DATE=$(oc get secret/$SECRET -n $PROJECT -o jsonpath='{.metadata.creationTimestamp}')
+        CREATE_EPOCH=$($DATE --date="${CREATE_DATE}" +"%s")
+        ELAPSED=$(expr $NOW_EPOCH - $CREATE_EPOCH)
+        ELAPSED_DAY=$(expr $ELAPSED / 86400)
+        if [[ $STRING =~ ^kubelet-client ]];
         then
-            if [ $1 == "csv" ];
+            # kubelet-client is automatically rotated every 30 days
+            if [ $ELAPSED_DAY -lt 30 ];
             then
-                printf "%s\n" "$PROJECT,$SECRET,$($DATE -d @$START_EPOCH),$($DATE -d @$END_EPOCH),$DAY_REMAIN"
+                output $1
             fi
         else
-            printf "%s\n" "==============================================="
-            printf "Project: %s\n" $PROJECT
-            printf "Secret: %s\n" $SECRET
-            printf "Created at: %s\n" "$($DATE -d @$START_EPOCH)"
-            printf "Expired after: %s\n" "$($DATE -d @$END_EPOCH)"
-            printf "Day remaining %s \n" $DAY_REMAIN
+            output $1
         fi
     done
 done
